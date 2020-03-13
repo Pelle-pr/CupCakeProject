2643a1095a216bcc07ada54057fcaf5856198156
Matt Thomsen <cph-mt312@cphbusiness.dk>
2020-03-13 15:48:54 +0100

    konto tilføjet mysql

--- src/main/java/DB/CupCake_DB (1).sql
+++ src/main/java/DB/CupCake_DB (1).sql
@@ -136,6 +136,7 @@ CREATE TABLE `user` (
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `role` varchar(45) NOT NULL,
+  `konto` int(11) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -158,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-13 15:44:26
+-- Dump completed on 2020-03-13 15:48:31
